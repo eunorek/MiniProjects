00a5fa239bfea8d07e0a763122c833a00dd53b42
YeEun Kim <snakefeet42@gmail.com>
2018-08-21 16:43:32 +0900

    recent progress

--- vending_machine/test_vm.py
+++ vending_machine/test_vm.py
@@ -1,9 +1,12 @@
 from vm import *
 
+def test_inition_change_should_be_zero():
+    Jim = VendingMachine()
+    assert "잔돈은 0 원입니다" == Jim.run("호에에에에에에엑")
 
-def test_insert_coin_and_check():
-    assert "잔액은 0원입니다" == run("잔액")
-    assert "100원을 넣었습니다" == run("동전 100")
-    assert "잔액은 100원입니다" == run("잔액")
-    assert "100원을 넣었습니다" == run("동전 100")
-    assert "잔액은 200원입니다" == run("잔액")
+# def test_insert_coin_and_check():
+#     assert "the change is: 0 won" == run("change")
+#     assert "inserted 100 won" == run("coin 100")
+#     assert "the change is: 100 won" == run("change")
+#     assert "inserted 100 won" == run("coin 100")
+#     assert "the change is: 200 won" == run("잔액")
